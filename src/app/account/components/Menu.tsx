1de9831a58b92fd7c1b276499242a7d2a695ce20
kedarnacha <kedaranugerahnachaputra@gmail.com>
2023-11-02 19:58:01 +0700

    update account

--- src/app/account/components/Menu.tsx
+++ src/app/account/components/Menu.tsx
@@ -1,15 +1,9 @@
-import { ChevronRight, CogIcon, FileSearch, HelpCircle, KeyIcon } from 'lucide-react';
-import Link from 'next/link';
+import { ChevronRight, CogIcon, FileSearch, HelpCircle } from 'lucide-react';
 import React from 'react';
 
 const Menu = () => {
   return (
     <div className="p-5">
-      <div className="flex cursor-pointer items-center space-x-4 border-b border-b-slate-200 px-5 py-2 transition-colors duration-150 hover:bg-slate-100">
-        <CogIcon className="h-5 w-5 text-orange-500" />
-        <div className="flex-1">Pengaturan</div>
-        <ChevronRight className="h-5 w-5 text-slate-500" />
-      </div>
       <div className="flex cursor-pointer items-center space-x-4 border-b border-b-slate-200 px-5 py-2 transition-colors duration-150 hover:bg-slate-100">
         <HelpCircle className="h-5 w-5 text-orange-500" />
         <div className="flex-1">Bantuan</div>
@@ -25,13 +19,6 @@ const Menu = () => {
         <div className="flex-1">Syarat Dan Ketentuan</div>
         <ChevronRight className="h-5 w-5 text-slate-500" />
       </div>
-      <Link href="/change-password">
-        <div className="flex cursor-pointer items-center space-x-4 border-b border-b-slate-200 px-5 py-2 transition-colors duration-150 hover:bg-slate-100">
-          <KeyIcon className="h-5 w-5 text-orange-500" />
-          <div className="flex-1">Ubah Password</div>
-          <ChevronRight className="h-5 w-5 text-slate-500" />
-        </div>
-      </Link>
     </div>
   );
 };
--- src/app/account/edit/page.tsx
+++ src/app/account/edit/page.tsx
@@ -21,6 +21,9 @@ import Navbar from '@/components/zis/Navbar';
 import useMutateEditAccount from '../hooks/useMutateEditAccount';
 import useQueryAccount from '../hooks/useQueryAccount';
 
+import { ChevronRight, KeyIcon } from 'lucide-react';
+import Link from 'next/link';
+
 const schema = z.object({
   nama: z.string().min(1),
   phone: z.string().min(1),
@@ -90,6 +93,13 @@ const EditPage = () => {
             </div>
           </div>
         </Form>
+        <Link href="/change-password">
+          <div className="flex mt-3 cursor-pointer items-center space-x-4 border-b border-b-slate-200 px-5 py-2 transition-colors duration-150 hover:bg-slate-100">
+            <KeyIcon className="h-5 w-5 text-orange-500" />
+            <div className="flex-1">Ubah Password</div>
+            <ChevronRight className="h-5 w-5 text-slate-500" />
+          </div>
+        </Link>
       </div>
 
       <div className="shadow-t-sm fixed inset-x-0 bottom-0 mx-auto flex w-full max-w-md items-center space-x-4 border-t border-t-slate-100 bg-white p-5">
