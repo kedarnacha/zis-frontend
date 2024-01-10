fdaaa4d16956e423cc17ef0163b20b27e58a78ba
kedarnacha <kedaranugerahnachaputra@gmail.com>
2024-01-10 12:10:52 +0700

    update nominal

--- src/schema/proposal.ts
+++ src/schema/proposal.ts
@@ -37,7 +37,10 @@ export const proposalSchema = z.object({
   status_rumah: z.number().optional(),
   status_pernikahan: z.number().optional(),
   jumlah_anak: z.string().optional(),
-  penghasilan_bulanan: z.string().optional(),
+  penghasilan_bulanan: z.string()
+  .refine((value) => /^[0-9]+$/.test(value), {
+    message: 'Masukkan nilai tanpa titik maupun koma',
+  }).optional(),
   nama_pasangan: z.string().optional(),
   pekerjaan: z.string().optional(),
   pendidikan_terakhir: z.number().optional(),
@@ -50,20 +53,38 @@ export const proposalSchema = z.object({
   tempat_mengajar: z.string().optional(),
   alamat_mengajar: z.string().optional(),
   sebagai_guru: z.string().optional(),
-  biaya_pendidikan_bulanan: z.string().optional(),
+  biaya_pendidikan_bulanan: z.string()
+  .refine((value) => /^[0-9]+$/.test(value), {
+    message: 'Masukkan nilai tanpa titik maupun koma',
+  }).optional(),
   jumlah_tanggungan: z.string().optional(),
   organisasi_yang_diikuti: z.string().optional(),
   nama_ayah: z.string().optional(),
   pekerjaan_ayah: z.string().optional(),
-  penghasilan_bulanan_ayah: z.string().optional(),
+  penghasilan_bulanan_ayah: z.string()
+  .refine((value) => /^[0-9]+$/.test(value), {
+    message: 'Masukkan nilai tanpa titik maupun koma',
+  }).optional(),
   nama_ibu: z.string().optional(),
   pekerjaan_ibu: z.string().optional(),
-  penghasilan_bulanan_ibu: z.string().optional(),
+  penghasilan_bulanan_ibu: z.string()
+  .refine((value) => /^[0-9]+$/.test(value), {
+    message: 'Masukkan nilai tanpa titik maupun koma',
+  }).optional(),
   jenis_bantuan_kesehatan: z.string().optional(),
   bantuan_pihak_lain: z.string().optional(),
-  nominal_bantuan: z.string().optional(),
-  biaya_hidup_bulanan: z.string().optional(),
-  dana_yang_diajukan: z.string().optional(),
+  nominal_bantuan: z.string()
+  .refine((value) => /^[0-9]+$/.test(value), {
+    message: 'Masukkan nilai tanpa titik maupun koma',
+  }).optional(),
+  biaya_hidup_bulanan: z.string()
+  .refine((value) => /^[0-9]+$/.test(value), {
+    message: 'Masukkan nilai tanpa titik maupun koma',
+  }).optional(),
+  dana_yang_diajukan: z.string()
+  .refine((value) => /^[0-9]+$/.test(value), {
+    message: 'Masukkan nilai tanpa titik maupun koma',
+  }).optional(),
   nama_pemberi_rekomendasi: z.string().min(1).optional(),
   alamat_pemberi_rekomendasi: z.string().min(1).optional(),
   no_telp_pemberi_rekomendasi: z.string().min(1).optional(),
