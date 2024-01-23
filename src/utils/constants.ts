9f42847a8782a26c25a012159157aee3697bbc36
kedarnacha <kedaranugerahnachaputra@gmail.com>
2024-01-23 15:37:13 +0700

    update fixing

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -9,7 +9,7 @@ export const ACCOUNT_QUERY_KEY = 'ACCOUNT';
 export const TYPE_MUSTAHIQ = 10;
 export const TYPE_MUZAKI = 11;
 
-export const ACCEPTED_SIZE = 2 * 1024 * 1024;
+export const ACCEPTED_SIZE = 3 * 1024 * 1024;
 export const ACCEPTED_IMAGE_TYPES = [
   'image/jpeg',
   'image/jpg',
